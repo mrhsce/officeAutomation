36501ab5c6002f38e09c4cd04d37a550018a7832
MrHsCE <mrhsce@gmail.com>
2015-12-26 18:36:10 +0330

    [edit] all roles are added to index

--- index.php
+++ index.php
@@ -17,8 +17,26 @@ if($_SESSION['login']){
         case 'contEmp':
             include_once 'roles/contractorEmployee.php';
             break;
+        case 'partTimeEmp':
+        	include_once 'roles/partTimeEmployee.php';
+        	break;
+        case 'ceo':
+        	include_once 'roles/ceo.php';
+        	break;
+        case 'director':
+        	include_once 'roles/director.php';
+        	break;
+        case 'humanRes':
+        	include_once 'roles/humanResource.php';
+        	break;
+        case 'officialEmp':
+        	include_once 'roles/officialEmployee.php';
+        	break;
+        case 'sysAdmin':
+        	include_once 'roles/sysAdmin.php';
+        	break;					
         default:
-            include_once 'roles/contractorEmployee.php';
+            include_once 'roles/officialEmployee.php';
 
     }
 }
