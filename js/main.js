6d3aee360f813b3a72fd3d52472a5d7aa6cfe3b7
Mohammad-Eslahi-Sani <mohammad_eslahi_sani@yahoo.com>
2015-12-26 17:41:35 +0330

    [New Code] showing statement

--- js/main.js
+++ js/main.js
@@ -7,11 +7,50 @@
 $(document).ready(function(){
     //$('form.login-form button.submit-button').on('click',submit);
 
+
+    //statement
+    $('button.show-statement').on('click',askForStatement);
 });
 
 
 
+function askForStatement(){
+    alert('ask for the statement');
 
+    var d={
+        task:'statement',
+        personalId:'10101001'
+    };
+    mainAjax(d,showStatement);
+}
+
+
+function showStatement(r){
+    $('div.show-statement').show();
+    $('div.main').hide();
+    alert('showing the statement');
+    $('div.show-statement table.personalInfo td.firstName').innerHTML = r['firstName'];
+    $('div.show-statement table.personalInfo td.lastName').innerHTML = r['lastName'];
+    $('div.show-statement table.personalInfo td.birthDate').innerHTML = r['birthDate'];
+    $('div.show-statement table.personalInfo td.sodoorPlace').innerHTML = r['sodoorPlace'];
+    $('div.show-statement table.personalInfo td.nationalId').innerHTML = r['nationalId'];
+    $('div.show-statement table.personalInfo td.maritalState').innerHTML = r['maritalState'];
+    $('div.show-statement table.personalInfo td.gender').innerHTML = r['gender'];
+    $('div.show-statement table.personalInfo td.eduLevel').innerHTML = r['eduLevel'];
+    $('div.show-statement table.contractInfo td.contractType').innerHTML = r['contractType'];
+    $('div.show-statement table.contractInfo td.postTitle').innerHTML = r['postTitle'];
+    $('div.show-statement table.contractInfo td.managerId').innerHTML = r['managerId'];
+    $('div.show-statement table.salaryInfo td.mScore').innerHTML = r['mScore'];
+    $('div.show-statement table.salaryInfo td.pScore').innerHTML = r['pScore'];
+    $('div.show-statement table.salaryInfo td.base').innerHTML = r['base'];
+    $('div.show-statement table.salaryInfo td.adding').innerHTML = r['adding'];
+    $('div.show-statement table.salaryInfo td.additional').innerHTML = r['additional'];
+    $('div.show-statement table.salaryInfo td.badClimate').innerHTML = r['badClimate'];
+    $('div.show-statement table.salaryInfo td.hardness').innerHTML = r['hardness'];
+    $('div.show-statement table.salaryInfo td.familyScore').innerHTML = r['familyScore'];
+    $('div.show-statement table.salaryInfo td.childrenNumber').innerHTML = r['childrenNumber'];
+    $('div.show-statement table.salaryInfo td.years').innerHTML = r['years'];
+}
 
 function submit(){
     var username = $('form.login-form input.username-input').val();
@@ -20,9 +59,6 @@ function submit(){
 }
 
 
-function showStatement(){
-
-}
 
 function mainAjax(d,sFunc,ajaxUrl,eFunc,ajaxType,ajaxAsync){
      if (typeof ajaxUrl === "undefined" || ajaxUrl === null) {
--- roles/contractorEmployee.php
+++ roles/contractorEmployee.php
@@ -14,8 +14,9 @@ else{
 		<title>contractor employee main page</title>
 	</head>
 	<body>
+	<div class="main">
 		<h3>welcome <?php if($_SESSION['login']['gender'] == "M") echo "Mr "; else echo "Ms";
-		?> <?php echo "".$_SESSION['login']['firstName']." ".$_SESSION['login']['lastName'] ?>;</h3>
+			?> <?php echo "".$_SESSION['login']['firstName']." ".$_SESSION['login']['lastName'] ?>;</h3>
 
 		<!-- Showing personal info -->
 		<button class="show-info" type="button" >Show personal info</button>
@@ -27,6 +28,98 @@ else{
 		<hr>
 
 		<button class="logout" type="button">Log out</button>
+
+	</div>
+
+
+	<div class="show-statement" style="display: none;">
+		<span>Personal Id</span>
+		<span class="personalId"></span>
+		<table class="personalInfo">
+			<thead>
+			<tr>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Birth Date</th>
+				<th>Sodoor Place</th>
+				<th>NationalId</th>
+				<th>Marital State</th>
+				<th>Gender</th>
+				<th>Children Number</th>
+				<th>Education Level</th>
+
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td class="firstName"></td>
+				<td class="lastName"></td>
+				<td class="birthDate"></td>
+				<td class="sodoorPlace"></td>
+				<td class="nationalId"></td>
+				<td class="maritalState"></td>
+				<td class="gender"></td>
+				<td class="childrenNumber"></td>
+				<td class="eduLevel"></td>
+			</tr>
+			</tbody>
+		</table>
+
+		<hr>
+
+		<table class="contractInfo">
+			<thead>
+			<tr>
+				<th>Contract Type</th>
+				<th>Post Title</th>
+				<th>Office Title</th>
+				<th>Manager Id</th>
+
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td class="contractType"></td>
+				<td class="postTitle"></td>
+				<td class="officeTitle"></td>
+				<td class="managerId"></td>
+			</tr>
+			</tbody>
+		</table>
+
+		<hr>
+
+		<table class="salaryInfo">
+			<thead>
+			<tr>
+				<th>Management Score</th>
+				<th>Post Score</th>
+				<th>Base</th>
+				<th>Adding</th>
+				<th>Additional</th>
+				<th>Bad Climate</th>
+				<th>Hardness</th>
+				<th>Family Score</th>
+				<th>Children Number</th>
+				<th>Years</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td class="mScore"></td>
+				<td class="pScore"></td>
+				<td class="base"></td>
+				<td class="adding"></td>
+				<td class="additional"></td>
+				<td class="badClimate"></td>
+				<td class="hardness"></td>
+				<td class="familyScore"></td>
+				<td class="childrenNumber"></td>
+				<td class="years"></td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
 
 	</body>
 	<?php
