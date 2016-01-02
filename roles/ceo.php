b017b594aab0c4b0833691d86197ff27a1dea880
Mohammad-Eslahi-Sani <mohammad_eslahi_sani@yahoo.com>
2016-01-02 23:02:59 +0330

    [Update] panels
    panel are changed from the navbar

--- roles/ceo.php
+++ roles/ceo.php
@@ -7,16 +7,6 @@ if(!isset($_SESSION['login'])){
 }
 else{
 ?>
-<html>
-<head >
-    <link href="css/bootstrap.min.css">
-    <link href="css/main.css">
-    <script type="application/javascript" src="js/jquery-2.1.4.js"></script>
-    <script type="application/javascript" src="js/main.js"></script>
-
-
-    <title>ceo page</title>
-</head>
 <body>
 
 
@@ -24,55 +14,54 @@ else{
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a class="navbar-brand" href="#">Office Automation</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
+
+                <?php if(!isset($_GET['managerPanel'])){ ?>
+                <li class="active"><a href="?home">Home</a></li>
+                <li ><a href="?managerPanel">Manager Panel <span class="sr-only">(current)</span></a></li>
+                <li><a type="button" class="btn btn-default">Add Employee</a></li>
+                <?php } else{ ?>
+                <li ><a href="?home">Home</a></li>
+                <li class="active"><a href="?managerPanel">Manager Panel <span class="sr-only">(current)</span></a></li>
+                <li><a type="button" class="btn btn-default">Add Employee</a></li>
+                <?php } ?>
             </ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
+                <li>
+                    <a style="color:cornflowerblue">
+                    Welcome <?php if($_SESSION['login']['gender'] == "M") echo "Mr "; else echo "Ms";
+                    ?> <?php echo "".$_SESSION['login']['firstName']." ".$_SESSION['login']['lastName'] ?>
+                    </a>
                 </li>
+                <li><a class="logout-link" href="#">Logout</a></li>
+
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
+
+<?php if(!!isset($_GET['managerPanel'])){ ?>
+    <!-- List all the employees -->
+    <button class="show-employeeList" type="button" >List all the employees</button>
+    <!-- Showing offices -->
+    <button class="show-offices" type="button" >Show offices</button>
+    <!-- Showing posts -->
+    <button class="show-posts" type="button" >Show Posts</button>
+    <!-- Showing basic tables -->
+    <button class="show-basicTables" type="button" >Show Basic tables</button>
+<?php } else{ ?>
+    <!-- Showing personal info -->
+    <button class="show-personalInfo" type="button" >Show personal info</button>
+    <!-- Showing statement -->
+    <button class="show-statement" type="button" >Show the statement</button>
+    <!-- Showing salaries -->
+    <button class="show-salaries" type="button" >Show salaries</button>
+<?php } ?>
 
 
 
