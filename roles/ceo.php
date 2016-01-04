cc804629bef2ff3681f61b59abcad9eb42523627
Mohammad-Eslahi-Sani <mohammad_eslahi_sani@yahoo.com>
2016-01-04 11:52:09 +0330

    [New] add employee form

--- roles/ceo.php
+++ roles/ceo.php
@@ -24,15 +24,15 @@ else{
                 <?php if(!isset($_GET['managerPanel'])){ ?>
                     <li class="active"><a href="?home">Home</a></li>
                     <li ><a href="?managerPanel">Manager Panel <span class="sr-only">(current)</span></a></li>
-                    <li><a type="button" class="btn btn-default">Add Employee</a></li>
-                    <?php if(!isset($_GET['home'])){ ?>
+                    <li><a type="button" href="?add" class="btn btn-default">Add Employee</a></li>
+                    <?php if(!isset($_GET['home']) and !isset($_GET['add'])){ ?>
                         <li><a type="button" class="btn btn-default export-btn">Export The Page</a></li>
                     <?php } ?>
                 <?php } else{ ?>
                     <li ><a href="?home">Home</a></li>
                     <li class="active"><a href="?managerPanel">Manager Panel <span class="sr-only">(current)</span></a></li>
-                    <li><a type="button" class="btn btn-default">Add Employee</a></li>
-                    <?php if(!isset($_GET['home'])){ ?>
+                    <li><a type="button"  href="?add" class="btn btn-default">Add Employee</a></li>
+                    <?php if(!isset($_GET['home']) and !isset($_GET['add'])){ ?>
                         <li><a type="button" class="btn btn-default export-btn">Export The Page</a></li>
                     <?php } ?>
                 <?php } ?>
@@ -575,8 +575,242 @@ else{
 
 
     </div>
+<?php } elseif(!!isset($_GET['add'])){ ?>
+    <div class="container-fluid">
+    <form class="add-employee-form" method="get">
+        <div class="container-fluid row">
+
+            <div class="container-fluid col col-lg-3">
+                <div class="row center-align">
+                    Personal Information
+                </div>
+
+                <hr>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="firstName">First Name</label>
+                    <input class="col col-lg-6" name="firstName" id="firstName" type="text">
+
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="lastName">Last Name</label>
+                    <input class="col col-lg-6" name="lastName" id="lastName" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="gender">Gender</label>
+                    <select class="col col-lg-6" name="gender" id="gender">
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                    </select>
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="nationalId">National ID</label>
+                    <input class="col col-lg-6" name="nationalId" id="nationalId" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="birthDate">Birth Date</label>
+                    <input class="col col-lg-6" name="birthDate" id="birthDate" type="date">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="sodoorPlace">Born City</label>
+                    <input class="col col-lg-6" name="sodoorPlace" id="sodoorPlace" type="text">
+                </div>
 
 
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="maritalStatus">Marital Status</label>
+                    <select class="col col-lg-6" name="maritalStatus" id="maritalStatus">
+                        <option value="M">Married</option>
+                        <option value="S">Single</option>
+                    </select>
+                </div>
+
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="childrenNumber">Children Number</label>
+                    <input class="col col-lg-6" name="childrenNumber" id="childrenNumber" type="number">
+
+                </div>
+
+
+
+            </div>
+
+            <div class="container-fluid col col-lg-3">
+                <div class="row center-align">
+                    Education Information
+                </div>
+
+                <hr>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="eduLevel">Education Level</label>
+                    <select class="col col-lg-6" name="eduLevel" id="eduLevel">
+                        <option value="Associate">Associate</option>
+                        <option value="Bachelor">Bachelor</option>
+                        <option value="Diploma">Diploma</option>
+                        <option value="Elementery">Elementery</option>
+                        <option value="Master">Master</option>
+                        <option value="Ph.D">Ph.D</option>
+                    </select>
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="field">Field</label>
+                    <input class="col col-lg-6" name="field" id="field" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="institute">Institute</label>
+                    <input class="col col-lg-6" name="institute" id="institute" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="graduationDate">Graduation Date</label>
+                    <input class="col col-lg-6" name="graduationDate" id="graduationDate" type="date">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="projectTitle">ProjectTitle</label>
+                    <input class="col col-lg-6" name="projectTitle" id="projectTitle" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="average">Average</label>
+                    <input class="col col-lg-6" name="average" id="average" type="number">
+                </div>
+
+
+
+            </div>
+
+            <div class="container col col-lg-3">
+                <div class="row center-align">
+                    Contract Information
+                </div>
+
+                <hr>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="contractId">Contract ID</label>
+                    <input class="col col-lg-6" name="contractId" id="contractId" type="text" value="10101010">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="personalId">Personal ID</label>
+                    <input class="col col-lg-6" name="personalId" id="personalId" type="text" value="10101010">
+                </div>
+
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="startDate">Start Date</label>
+                    <input class="col col-lg-6" name="startDate" id="startDate" type="date">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="expireDate">Expire Date</label>
+                    <input class="col col-lg-6" name="expireDate" id="expireDate" type="date">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="postId">Post</label>
+                    <select class="col col-lg-6" name="postId" id="postId">
+                        <option value="01">Developer</option>
+                        <option value="02">Service</option>
+                        <option value="03">Operator</option>
+                        <option value="04">High Developer</option>
+                        <option value="05">Analyzer</option>
+                        <option value="06">High Analyzer</option>
+                        <option value="07">Net Engineer</option>
+                        <option value="08">Hardware Engineer</option>
+                    </select>
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="officeId">Office</label>
+                    <select class="col col-lg-6" name="officeId" id="officeId">
+                        <option value="01">User Interface</option>
+                        <option value="02">Back</option>
+                        <option value="03">Security</option>
+                        <option value="04">Official</option>
+                        <option value="05">Android</option>
+                    </select>
+                </div>
+
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="managerId">Manager</label>
+                    <select class="col col-lg-6" name="managerId" id="managerId">
+                        <option value="0">Chief</option>
+                        <option value="1">Director</option>
+                        <option value="2">Assistant</option>
+                        <option value="3">Office Manager</option>
+                        <option value="4">Team Header</option>
+                    </select>
+                </div>
+
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="contractType">Contract Type</label>
+                    <select class="col col-lg-6" name="contractType" id="contractType">
+                        <option value="formal">Formal</option>
+                        <option value="contractual">Contractual</option>
+                        <option value="partTime">Part Time</option>
+                    </select>
+                </div>
+
+
+
+            </div>
+
+
+
+
+            <div class="container-fluid col col-lg-3">
+                <div class="row center-align">
+                    User Information
+                </div>
+                <hr>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="username">Username</label>
+                    <input class="col col-lg-6" name="username" id="username" type="text">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="pass">Password</label>
+                    <input class="col col-lg-6" name="pass" id="pass" type="password">
+                </div>
+
+                <div class="container-fluid row">
+                    <label class="right-align col col-lg-6" for="role">Role</label>
+                    <select class="col col-lg-6" name="role" id="role">
+                        <option value="ceo">Manager</option>
+                        <option value="contractorEmployee">Employee</option>
+                    </select>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+        <hr>
+
+        <div class="container-fluid center-align row">
+            <button type="submit">Register</button>
+        </div>
+
+
+        <!--        <div class="row">  </div>-->
+    </form>
+    </div>
 <?php } else{ ?>
 
     <div class="container ceo-home-main">
